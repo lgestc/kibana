a739811eb33eb6fffedc37cc8ba7290894af8393
lgestc <lukasz.gmys@elastic.co>
2026-05-14 10:56:38 +0200

    fix lint issues

--- x-pack/platform/plugins/shared/cases/server/saved_object_types/index.ts
+++ x-pack/platform/plugins/shared/cases/server/saved_object_types/index.ts
@@ -61,6 +61,7 @@ export const registerSavedObjects = ({
   if (config.templates?.enabled) {
     // eslint-disable-next-line @kbn/eslint/no_conditional_saved_object_type_registration -- TODO: remove conditional registration; tracked for follow-up PR
     core.savedObjects.registerType(caseTemplateSavedObjectType);
+    // eslint-disable-next-line @kbn/eslint/no_conditional_saved_object_type_registration -- caseFieldDefinitionSavedObjectType is part of the templates feature which is behind a feature flag
     core.savedObjects.registerType(caseFieldDefinitionSavedObjectType);
   }
   if (config.attachments?.enabled) {
